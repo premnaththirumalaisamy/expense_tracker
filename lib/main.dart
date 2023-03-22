e2874b21d07670b0455a36ebf08c92790d326bad
Prem <prem@Prems-MacBook-Pro.local>
2023-03-22 23:11:47 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Expense Tracker'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
